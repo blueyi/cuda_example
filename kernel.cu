41f33f2ad41b37b899adcce6f0e8ee9bbfc57cad
blueyi <blueyiniu@qq.com>
2015-06-12 17:31:56 +0800

    first commit

---
+++ kernel.cu
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "book.h"
+
+__global__ void add(int a, int b, int *c)
+{
+	*c = a + b;
+}
+
+int main(void)
+{
+	int c;
+	int *dev_c;
+	HANDLE_ERROR(cudaMalloc((void**)&dev_c, sizeof(int)));
+
+	add << <1, 1 >> >(2, 7, dev_c);
+
+	printf("2 + 7 = %d\n", &dev_c);
+
+	HANDLE_ERROR(cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost));
+	printf("2 + 7 = %d\n", c);
+	cudaFree(dev_c);
+	return  0;
+}
